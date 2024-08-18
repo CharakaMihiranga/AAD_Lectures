12836ef1c2d34a2d4cfb725518d02231943aaeaf
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-08-18 10:39:43 +0530

    custom headers with regex patterns, get data as a map, get data as a MultiValueMap

--- SpringWeb_Intro/src/main/java/lk/ijse/gdse/springweb/springweb_intro/controller/demoController.java
+++ SpringWeb_Intro/src/main/java/lk/ijse/gdse/springweb/springweb_intro/controller/demoController.java
@@ -1,10 +1,12 @@
 package lk.ijse.gdse.springweb.springweb_intro.controller;
 
 import jakarta.servlet.http.HttpServlet;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequestMapping("/demo")
 //The @RequestMapping annotation in Spring Web is used to map web requests to specific handler classes and/or handler methods.
@@ -41,4 +43,48 @@ public class DemoController {
     public String otherTest(){
         return "Other test";
     }
+    //Methods that can get data from the client using headers
+    @PostMapping(params = {"name","quantity"} )
+    public String paramData(@RequestParam("name") String param1, @RequestParam("quantity") String param2) {
+        return "Param data is "+ param1 + " and "+ param2;
+    }
+    //Custom headers with regex pattern
+    @GetMapping(value = "patternRegex/{id:C\\d{3}}", headers = "X-number") //http://localhost:8080/SpringWebIntro/demo/patternRegex/C001 and add a header names X-number
+    public String pathVariableWithRegexWithHeaders(@PathVariable("id") String id, @RequestHeader("X-number") String number){
+        return "Pattern Regex is : "+id + " and header number is : "+number;
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE) //http://localhost:8080/SpringWebIntro/demo and set the header content type as application/json
+    public String saveJSON(){
+        return "JSON data saved";
+    }
+    @PostMapping("/dynamic/{value:\\d{2}}") //http://localhost:8080/SpringWebIntro/demo/dynamic/12
+    public ResponseEntity<String> returnDynamicData( @PathVariable("value")int incomingValue ) {
+        //ResponseEntity used to send additional information with the response
+        if (incomingValue % 2 ==0){
+            return ResponseEntity.ok("Dynamic data is even");
+        }else {
+            return ResponseEntity.ok("Dynamic data is odd");
+        }
+    }
+    // get data as a map
+    @PostMapping(value = "/params", params = { "id", "desc"}) //http://localhost:8080/SpringWebIntro/demo/params?id=1&desc=Test
+    public String handleMaps ( @RequestParam("id") String id, @RequestParam("desc") String desc, @RequestParam Map<String, String> params) {
+        System.out.println(params);
+        return "Handle Maps with params: "+params;
+    }
+    // get data as a MultiValueMap
+    @PostMapping(value = "/multimapparams", params = { "id", "desc"}) //http://localhost:8080/SpringWebIntro/demo/multimapparams?id=1&desc=Test&id=2&desc=Test2
+    public String handleWithMultiMaps ( @RequestParam("id") String id, @RequestParam("desc") String desc, @RequestParam MultiValueMap<String, String> params) {
+        System.out.println(params);
+        return "Handle multi value maps with params: "+params;
+    }
 }
+
+
+//Headers : Headers used to pass additional information with the request or response. It is used to pass the information about
+// the client browser, the type of the body content, etc. The headers are passed in the form of key-value pairs.
+
+//Consumes : The @Consumes annotation is used to specify the MIME media types of representations a resource can consume that were sent by the client.
+
+//MultiValueMap : MultiValueMap is a sub-interface of Map that stores multiple values. It is used to store multiple values in a single key.
+// It is used to store multiple values in a single key. It's a part of spring framework.
