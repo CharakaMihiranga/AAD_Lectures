a687592cfb06d8cab309be45d215c087aeefdc40
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-10-14 23:08:33 +0530

    Add AuthenticationProvider

--- NoteTakerV2(AAD-Phase-3)/src/main/java/lk/ijse/gdse/springboot/notetakerV2/config/SecurityConfig.java
+++ NoteTakerV2(AAD-Phase-3)/src/main/java/lk/ijse/gdse/springboot/notetakerV2/config/SecurityConfig.java
@@ -4,10 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,5 +31,22 @@ public class SecurityConfig {
                 .authenticationProvider()
                 .addFilterBefore();
         return http.build();
+    }
+    //Password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    //Auth provider
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider dap =
+                new DaoAuthenticationProvider();
+        dap.setUserDetailsService();
+        dap.setPasswordEncoder(passwordEncoder());
+        return dap;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
