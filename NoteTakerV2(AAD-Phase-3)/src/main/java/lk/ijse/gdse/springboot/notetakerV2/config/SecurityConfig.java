d919de6299e0f43a7d9017a92fb47805bc5d1f1f
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-10-14 23:04:21 +0530

    Add SecurityConfig - creating SecurityFilterChain

--- NoteTakerV2(AAD-Phase-3)/src/main/java/lk/ijse/gdse/springboot/notetakerV2/config/SecurityConfig.java
+++ NoteTakerV2(AAD-Phase-3)/src/main/java/lk/ijse/gdse/springboot/notetakerV2/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package lk.ijse.gdse.springboot.notetakerV2.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests( req ->
+                        req.requestMatchers("api/v1/auth/**").permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider()
+                .addFilterBefore();
+        return http.build();
+    }
 }
